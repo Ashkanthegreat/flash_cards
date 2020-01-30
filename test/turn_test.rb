bd2a729be93045ffca971d3076d0e5b618676263
Ashkan Abbasi <airashkan@yahoo.com>
2020-01-30 13:01:03 -0700

    Clean Turn Test

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -3,8 +3,7 @@ require 'minitest/pride'
 require './lib/card'
 require './lib/turn'
 
-# card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-# turn = Turn.new("Juneau", card)
+
 
 class TurnTest < Minitest::Test
   def test_it_exists
@@ -40,45 +39,8 @@ class TurnTest < Minitest::Test
     turn = Turn.new("Juneau", card)
 
     assert_equal "Correct!", turn.feedback
-  end   
+  end
 
 
 
 end
-
-
-
-
-
-
-
-
-
-
-# pry(main)> turn.feedback
-# #=> "Correct!"
-# We also want to make sure that incorrect guesses are handled properly.
-#
-# pry(main)> require './lib/turn'
-# #=> true
-#
-# pry(main)> require './lib/card'
-# #=> true
-#
-# pry(main)> card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
-# #=> #<Card:0x007ffdf1820a90 @answer="Mercury", @question="Which planet is closest to the sun?", @category=:STEM>
-#
-# pry(main)> turn = Turn.new("Saturn", card)
-# #=> #<Turn:0x00007f998413ee60 @card=#<Card:0x007ffdf1820a90 @answer="Mercury", @question="Which planet is closest to the sun?", @category=:STEM>, @guess="Saturn">
-#
-# pry(main)> turn.card
-# => #<Card:0x007ffdf1820a90 @answer="Mercury", @question="Which planet is closest to the sun?", @category=:STEM>
-#
-# pry(main)> turn.guess
-# => "Saturn"
-#
-# pry(main)> turn.correct?
-# => false
-#
-# pry(main)> turn.feedback
-# => "Incorrect."
