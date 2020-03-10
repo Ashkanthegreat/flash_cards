3b3cd798442210ec0a395a1f5bc701444bd72a0b
Ashkan Abbasi <airashkan@yahoo.com>
2020-03-09 20:50:06 -0600

    Complete deck and deck test

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,21 +1,17 @@
 class Deck
   attr_reader :cards
 
-  def initialize(cards = [])
+  def initialize(cards)
     @cards = cards
   end
-  
+
   def count
     @cards.size
   end
 
-  def cards_in_category
-    # cards.map {|cards| return card if card.category == category}
-    category_array = []
-    cards.each do |card|
-      if card.category == category
-        category_array << card
-      end
-    end
+  def cards_in_category(category)
+    @cards.select { |card| card.category == category }
   end
+
+
 end
