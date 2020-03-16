abe8fa89299b4bad7f5c24cbc7481f06d66afc60
Ashkan Abbasi <airashkan@yahoo.com>
2020-03-15 23:39:36 -0600

    Make Percent Correct by Category test and method, and finish iteration 2

--- lib/round.rb
+++ lib/round.rb
@@ -23,4 +23,13 @@ class Round
      @turns.select { |turn| turn.correct? && turn.card.category == category}.count
   end
 
+  def percent_correct
+    (number_correct.to_f / @turns.count.to_f) * 100
+  end
+
+  def percent_correct_by_category(category)
+    (number_correct_by_category(category) / @turns.count {|turn|
+    turn.card.category == category}.to_f * 100).round
+  end
+
 end
--- test/round_test.rb
+++ test/round_test.rb
@@ -64,6 +64,18 @@ class RoundTest < Minitest::Test
     assert_equal 0, @round.number_correct_by_category(:STEM)
   end
 
+  def test_peecent_correct
+    @round.take_turn("Juneau")
+    @round.take_turn("Venus")
+    assert_equal 50.0, @round.percent_correct
+  end
+
+  def test_peecent_correct_by_category
+    @round.take_turn("Juneau")
+    @round.take_turn('Mars')
+    assert_equal 100.0, @round.percent_correct_by_category(:Geography)
+    assert_equal @card_3, @round.current_card  
+  end
 
 
 end
