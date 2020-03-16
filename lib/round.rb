8218faad1092e8649e059abed8e00e85e17fa2e5
Ashkan Abbasi <airashkan@yahoo.com>
2020-03-15 22:08:26 -0600

    Make Take Turn test and method

--- lib/round.rb
+++ lib/round.rb
@@ -1,7 +1,22 @@
 class Round
-  attr_reader :deck
+  attr_reader :deck, :turns, :current_card
 
   def initialize(deck)
     @deck = deck
+    @turns = []
+    @number_correct = 0
   end
+
+  def current_card
+    @deck.cards[0]
+  end
+
+  def take_turn(guess)
+    turn = Turn.new(guess, current_card)
+    @number_correct += 1 unless turn.correct? != true
+    @turns << turn
+    @deck.cards.shift()
+    turn
+  end
+
 end
--- test/round_test.rb
+++ test/round_test.rb
@@ -11,7 +11,7 @@ class RoundTest < Minitest::Test
     @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-    Deck.new([@card_1, @card_2, @card_3])
+    @deck = Deck.new([@card_1, @card_2, @card_3])
     @round = Round.new(@deck)
   end
 
@@ -20,7 +20,25 @@ class RoundTest < Minitest::Test
   end
 
   def test_it_has_attributes
-    assert_equal @deck, @round.deck 
+    assert_equal @deck, @round.deck
+    assert_equal [], @round.turns
   end
+
+  def test_it_has_current_card
+    assert_equal @card_1, @round.current_card
+  end
+
+  def test_it_can_take_turn
+    new_turn = @round.take_turn('Juneau')
+    assert_equal "Juneau", new_turn.guess
+    assert_equal Turn, new_turn.class
+    assert_equal true, new_turn.correct?
+    assert_equal @card_1, new_turn.card
+    assert_equal :Geography, new_turn.card.category
+  end
+
+  
+
+
 
 end
